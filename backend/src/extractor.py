8581cfd5b4feccba51b5d2e39e87eb1753fe33ce
Ayush Senapati <ayushsenapati123@gmail.com>
2024-06-07 22:56:36 +0530

    integrated prescription and patient parser

--- backend/src/extractor.py
+++ backend/src/extractor.py
@@ -4,6 +4,9 @@ import pytesseract
 import utils
 from dotenv import load_dotenv
 
+from parser_prescription import PrescriptionParser
+from parser_patient_details import PatientDetailsParser
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -17,18 +20,16 @@ POPPLER_PATH = r'C:\poppler-24.02.0\Library\bin'
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def extract(file_path, file_format):
-    # Construct the full path using BASE_DIR and the relative file_path
     full_path = os.path.join(BASE_DIR, file_path)
 
-    # Print the paths for debugging
     print("Base Directory:", BASE_DIR)
     print("Relative Path:", file_path)
     print("Full Path:", full_path)
 
-    # Check if file exists
     if not os.path.exists(full_path):
         raise FileNotFoundError(f"The file {full_path} does not exist.")
     
+    #! step 1: extracting text from pdf file
     try:
         # Extracting text from pdf file
         pages = convert_from_path(full_path, poppler_path=POPPLER_PATH)
@@ -37,15 +38,30 @@ def extract(file_path, file_format):
     
     document_text = ""
     
-    for page in pages:
-        try:
-            processed_image = utils.preprocess_image(page)
-            text = pytesseract.image_to_string(processed_image, lang='eng')
-            document_text += '\n' + text
-        except Exception as e:
-            raise Exception(f"Failed to process image with Tesseract: {e}")
+    # for page in pages:
+    #     try:
+    #         processed_image = utils.preprocess_image(page)
+    #         text = pytesseract.image_to_string(processed_image, lang='eng')
+    #         document_text += '\n' + text
+    #     except Exception as e:
+    #         raise Exception(f"Failed to process image with Tesseract: {e}")
 
-    return document_text   
+    if len(pages)>0:
+        page = pages[0]
+        processed_image = utils.preprocess_image(page)
+        text = pytesseract.image_to_string(processed_image, lang='eng')
+        document_text = '\n' + text
+
+    #! step 2: extract fields from text
+    if file_format == 'prescription':
+        extracted_data = PrescriptionParser(document_text).parse()
+    elif file_format == 'patient_details':
+        extracted_data = PatientDetailsParser(document_text).parse()
+    else:
+        raise Exception(f"Invalid document format: {file_format}")
+
+    return extracted_data
+
 
 if __name__ == '__main__':
     try:
